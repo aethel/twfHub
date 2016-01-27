0f283a82c80d0dd201ef62398b1adfc64c9c53c3
aethel <mpankiewicz@infusion.com>
2016-01-27 15:35:28 +0100

    ie fix

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,9 +29,9 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-scroll/angular-scroll.js',
+      'bower_components/angular-video-bg/angular-video-bg.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/waypoints/lib/noframework.waypoints.js',
-      'bower_components/angular-video-bg/angular-video-bg.min.js',
       // endbower
       "app/scripts/**/*.js",
       "test/mock/**/*.js",
